bd054cc9e96666163d94d306faf1b4b54216c301
Vejay Drepaul <vejayddrepaul@gmail.com>
2021-12-12 22:03:11 -0500

    Grayscale added with -g flag

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <getopt.h>
 #include <string>
 
 void show_image(std::string image_file);
+void grayscale_image(std::string image_file);
 
 int main(int argc, char *argv[])
 {
@@ -15,9 +17,12 @@ int main(int argc, char *argv[])
     while((c = getopt(argc, argv, "fgbc:")) != 1) {
         switch(c) {
             case 'f':
-                show_image(std::string(argv[2]));
+                show_image(optarg); //std::string(argv[2])
                 break;
             case 'g':
+                grayscale_image(argv[2]);
+                break;
+            case '?':
                 break;
             default:
                 abort();
@@ -35,7 +40,22 @@ void show_image(std::string image_file) {
         std::cout << "Could not read image: " << image_file << std::endl;
     }
 
-    cv::imshow("Display Window", image);
+    cv::imshow(image_file, image);
+    int k = cv::waitKey(0);
+    if(k == 's') {
+        cv::imwrite(image_file, image);
+    }
+}
+
+void grayscale_image(std::string image_file) {
+    cv::Mat image = cv::imread(image_file, cv::IMREAD_GRAYSCALE);
+
+    if(image.empty()) {
+        std::cout << "Could not read image: " << image_file << std::endl;
+    }
+
+    std::string window_name = image_file + " - Grayscaled";
+    cv::imshow(window_name, image);
     int k = cv::waitKey(0);
     if(k == 's') {
         cv::imwrite(image_file, image);
